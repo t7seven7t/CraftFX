98f13fd5c753d6c09b751aca659947044739bada
t7seven7t <t7seven7t@gmail.com>
2016-02-03 13:44:21 -0900

    Add a method to update items and fix the bug with removing items inadvertently caused by changing where the NBT id is stored

--- src/main/java/net/t7seven7t/craftfx/listener/PlayerListener.java
+++ src/main/java/net/t7seven7t/craftfx/listener/PlayerListener.java
@@ -1,50 +1,98 @@
 package net.t7seven7t.craftfx.listener;
 
 import net.t7seven7t.craftfx.CraftFX;
-import net.t7seven7t.craftfx.nms.AttributeModifier;
+import net.t7seven7t.craftfx.item.ItemDefinition;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
-import java.util.List;
+import java.util.ListIterator;
+import java.util.Optional;
 
 /**
  *
  */
 public class PlayerListener implements Listener {
 
+    private final CraftFX fx = CraftFX.instance();
+
     @EventHandler(ignoreCancelled = true)
-    public void onInventoryMoveItem(InventoryMoveItemEvent event) {
-        if (!CraftFX.instance().getConfig().getBoolean("remove-unregistered-items", false)) return;
-        if (!(event.getDestination() instanceof PlayerInventory)
-                && !(event.getSource() instanceof PlayerInventory)) return;
-        List<AttributeModifier> modifierList = CraftFX.instance().getNmsInterface()
-                .getAttributeModifiers(event.getItem());
-        if (modifierList.stream().anyMatch(m -> m.getName().equals("craftfx.item"))) {
-            event.setCancelled(true);
-            final ItemStack item = event.getItem().clone();
-            item.setAmount(Integer.MAX_VALUE);
-            final Inventory source = event.getSource();
-            Bukkit.getScheduler().runTask(CraftFX.plugin(), () -> source.removeItem(item));
+    public void onPlayerPickupItem(PlayerPickupItemEvent event) {
+        if (!fx.getConfig().getBoolean("remove-unregistered-items")
+                && !fx.getConfig().getBoolean("update-items")) return;
+        try {
+            final ItemStack item = event.getItem().getItemStack();
+            final String id = fx.getNmsInterface().getCraftFXId(item);
+            if (id == null || id.isEmpty()) return;
+            final Optional<ItemDefinition> opt = fx.getItemRegistry().getDefinition(id);
+            if (opt.isPresent()) {
+                if (fx.getConfig().getBoolean("update-items")) {
+                    // update item
+                    final ItemStack i = opt.get().getItem();
+                    i.setAmount(item.getAmount());
+                    i.setDurability(item.getDurability());
+                    event.getItem().setItemStack(i);
+                }
+            } else if (fx.getConfig().getBoolean("remove-unregistered-items")) {
+                // remove item
+                event.setCancelled(true);
+                final Item i = event.getItem();
+                Bukkit.getScheduler().runTask(CraftFX.plugin(), i::remove);
+            }
+        } catch (UnsupportedOperationException e) {
+            // ignore
         }
     }
 
     @EventHandler(ignoreCancelled = true)
-    public void onPlayerPickupItem(PlayerPickupItemEvent event) {
-        if (!CraftFX.instance().getConfig().getBoolean("remove-unregistered-items", false)) return;
-        List<AttributeModifier> modifierList = CraftFX.instance().getNmsInterface()
-                .getAttributeModifiers(event.getItem().getItemStack());
-        if (modifierList.stream().anyMatch(m -> m.getName().equals("craftfx.item"))) {
-            event.setCancelled(true);
-            final Item item = event.getItem();
-            Bukkit.getScheduler().runTask(CraftFX.plugin(), item::remove);
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        final InventoryView view = event.getView();
+        if (view.getTopInventory() == null
+                || view.getTopInventory() instanceof PlayerInventory) return;
+        updateItems(view.getTopInventory());
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        updateItems(event.getPlayer().getInventory());
+    }
+
+    private void updateItems(Inventory inventory) {
+        if (!fx.getConfig().getBoolean("remove-unregistered-items")
+                && !fx.getConfig().getBoolean("update-items")) return;
+        final boolean updateItems = fx.getConfig().getBoolean("update-items");
+        final boolean removeItems = fx.getConfig().getBoolean("remove-unregistered-items");
+        try {
+            ListIterator<ItemStack> it = inventory.iterator();
+            while (it.hasNext()) {
+                final ItemStack item = it.next();
+                final String id = fx.getNmsInterface().getCraftFXId(item);
+                if (id == null || id.isEmpty()) continue;
+                final Optional<ItemDefinition> opt = fx.getItemRegistry().getDefinition(id);
+                if (opt.isPresent()) {
+                    if (updateItems) {
+                        // update item
+                        final ItemStack i = opt.get().getItem();
+                        i.setAmount(item.getAmount());
+                        i.setDurability(item.getDurability());
+                        it.set(i);
+                    }
+                } else if (removeItems) {
+                    // remove item
+                    it.set(null);
+                }
+            }
+        } catch (UnsupportedOperationException e) {
+            // ignore
         }
     }
 
