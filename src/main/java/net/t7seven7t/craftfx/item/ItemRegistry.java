66a2f99745f7956f7fe4699fca0a1aa5f7a1b742
t7seven7t <t7seven7t@gmail.com>
2016-02-18 20:45:51 -0900

    fix logic error in isSimilar comparison

--- src/main/java/net/t7seven7t/craftfx/item/ItemRegistry.java
+++ src/main/java/net/t7seven7t/craftfx/item/ItemRegistry.java
@@ -172,9 +172,10 @@ public class ItemRegistry {
         }
 
         try {
-            String id = CraftFX.instance().getNmsInterface().getCraftFXId(item1);
-            if (id != null && !id.isEmpty()) {
-                return id.equals(CraftFX.instance().getNmsInterface().getCraftFXId(item2));
+            final String id1 = CraftFX.instance().getNmsInterface().getCraftFXId(item1);
+            final String id2 = CraftFX.instance().getNmsInterface().getCraftFXId(item2);
+            if (id1 != null || id2 != null) {
+                return id1 != null && id1.equals(id2);
             }
         } catch (UnsupportedOperationException e) {
             // ignore & use display name check instead
